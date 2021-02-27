0fa243e68c28e4d90f6706a176689ae2176da767
Logan <loganemakf@gmail.com>
2021-02-27 12:26:27 -0500

    Moved GUI setup into private function & renamed process buton

--- src/GUI/MainWindow.py
+++ src/GUI/MainWindow.py
@@ -15,12 +15,20 @@ class MainWindow:
         self.window.title("Auction .csv Processor GUI v0.1")
         self.window.resizable(FALSE, FALSE)
 
+        self._setup_GUI()
+
+        self.window.protocol("WM_DELETE_WINDOW", self.shutdown_ttk_repeat)
+        self.window.mainloop()
+
+
+    def _setup_GUI(self):
         self.mainframe = ttk.Frame(self.window, padding="30 15")
         self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         self.window.columnconfigure(0, weight=1)
         self.window.rowconfigure(0, weight=1)
 
-        self.lbl_instruct = ttk.Label(self.mainframe, text="Select an AuctionFlex-exported .csv file to process", anchor="center")
+        self.lbl_instruct = ttk.Label(self.mainframe, text="Select an AuctionFlex-exported .csv file to process",
+                                      anchor="center")
         self.lbl_instruct.grid(column=0, row=0, columnspan=3)
 
         self.src_entry_text = StringVar()
@@ -31,10 +39,11 @@ class MainWindow:
         self.source_entry.grid(column=0, row=1, columnspan=2, sticky=(W, E))
         self.src_entry_default_color = self.source_entry.cget("foreground")
 
-        self.src_button = ttk.Button(self.mainframe, text="Browse", command=self.get_source_file).grid(column=2, row=1, sticky=W)
+        self.src_button = ttk.Button(self.mainframe, text="Browse", command=self.get_source_file).grid(column=2, row=1,
+                                                                                                       sticky=W)
 
         self.step2_frame = ttk.Frame(self.mainframe, padding=5)
-        self.step2_frame.grid(column=0, row=2, columnspan=3, sticky=(E,W))
+        self.step2_frame.grid(column=0, row=2, columnspan=3, sticky=(E, W))
         self.step2_frame.columnconfigure(0, weight=1)
         self.step2_frame.columnconfigure(1, weight=1)
         self.step2_frame.columnconfigure(2, weight=1)
@@ -46,8 +55,8 @@ class MainWindow:
                                       command=self.opensettings)
         self.sett_button.grid(column=1, row=0, sticky=E)
         self.dest_path = ""
-        self.dest_button = ttk.Button(self.step2_frame, text="Process", state=["disabled"], command=self.getdestfile)
-        self.dest_button.grid(column=3, row=0, sticky=W)
+        self.proc_button = ttk.Button(self.step2_frame, text="Process", state=["disabled"], command=self.getdestfile)
+        self.proc_button.grid(column=3, row=0, sticky=W)
 
         # progBar has 100 steps by default; update with currProg.set(val)
         self.curr_prog = DoubleVar()
@@ -57,9 +66,6 @@ class MainWindow:
         # add some padding around each UI element in mainframe
         for child in self.mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
-
-        self.window.protocol("WM_DELETE_WINDOW", self.shutdown_ttk_repeat)
-        self.window.mainloop()
 
 
     def get_source_file(self) -> None:
@@ -81,25 +87,26 @@ class MainWindow:
             # make sure we can open the selected file
             if not CSVProc.test_open(self.src_path):
                 self._entry_box_message("Unable to open selected file :(")
-                self.dest_button.state(["disabled"])
+                self.proc_button.state(["disabled"])
                 self.sett_button.state(["disabled"])
             else:
                 # print filename in Entry field next to 'Save' button
                 self._entry_box_message(f)
-                self.dest_button.state(["!disabled"])
+                # TODO: proc button should be enabled after setting verification
+                # self.proc_button.state(["!disabled"])
                 self.sett_button.state(["!disabled"])
         except RuntimeError:
             self._entry_box_message("No file selected")
             self.sett_button.state(["disabled"])
-            self.dest_button.state(["disabled"])
+            self.proc_button.state(["disabled"])
         except TypeError:
             self._entry_box_message(f"Invalid filetype: {ext}", is_error_msg=True)
             self.sett_button.state(["disabled"])
-            self.dest_button.state(["disabled"])
+            self.proc_button.state(["disabled"])
         except:
             self._entry_box_message("Unknown error =/")
             self.sett_button.state(["disabled"])
-            self.dest_button.state(["disabled"])
+            self.proc_button.state(["disabled"])
             pass
 
 
