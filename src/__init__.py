7ac9627c93ff7cb6e47ef7219dbdb5f183a6df43
Logan <loganemakf@gmail.com>
2021-03-15 18:25:37 -0400

    Added package-level constants for config.json file field names.

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# PROGRAM CONSTANTS
+# -----------------
+# Config file dictionary keys
+class CONF:
+    BP_COND = "bp_condition"
+    USING_BP_COND = "using_bp_condition"
+    CALC_STARTBID = "calculate_startbid"
+    CALC_EMPTY_STARTBIDS = "calculate_empty_startbids_only"
