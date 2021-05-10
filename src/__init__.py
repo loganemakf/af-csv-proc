1e06f4e5a7da4ece69265ab24cb1a4f21acbb329
Logan <loganemakf@gmail.com>
2021-05-09 21:46:13 -0400

    Added try-pass wrapper func for csv processing steps

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,22 @@
 # PROGRAM CONSTANTS
 # -----------------
-# Config file dictionary keys
 class C:
     PROGRAM_NAME = "AF .csv Processor v0.1"
 
+
+# Config file dictionary keys
 class CONF:
     BP_COND = "bp_condition"
     USING_BP_COND = "using_bp_condition"
     CALC_STARTBID = "calculate_startbid"
     CALC_EMPTY_STARTBIDS = "calculate_empty_startbids_only"
+
+
+def try_pass(func):
+    def wrapper(*args):
+        try:
+            func(*args)
+        except KeyError:
+            pass
+
+    return wrapper
