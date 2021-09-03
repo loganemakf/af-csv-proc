f7370deca09d378fb42904db59c9c3f7e5106707
Logan <loganemakf@gmail.com>
2021-09-03 15:06:44 -0400

    Added license header

--- main.py
+++ main.py
@@ -2,7 +2,20 @@
 # af-csv-proc - Post-processor for exported auction catalogs
 # Copyright (C) 2021  Logan Foster
 #
-# Run this file to launch the GUI
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Run this file ("python3 main.py") to launch the GUI
 
 import tkinter as tk
 from src.GUI.MainWindow import MainWindow
